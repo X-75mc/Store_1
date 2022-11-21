aadb259dda5b21dc11ee558b433d42c7a6e0784b
Artem Osypenko <artoss1234@gmail.com>
2022-11-21 22:07:18 +0200

    first commit

---
+++ cart.js
@@ -0,0 +1,67 @@
+const cartWrapper = document.querySelector('.cart-wrapper');
+
+window.addEventListener('click', function (event){
+
+    if (event.target.hasAttribute('data-cart')){
+        const card = event.target.closest('.card');
+
+        const productInfo = {
+            id: card.dataset.id,
+            imgSrc: card.querySelector('.product-img').getAttribute('src'),
+            title: card.querySelector('.item-title').innerText,
+            price: card.querySelector('.price__currency').innerText,
+			counter: card.querySelector('[data-counter]').innerText,
+        };
+
+        const itemInCart = cartWrapper.querySelector(`[data-id="${productInfo.id}"]`);
+
+        if (itemInCart) {
+			const counterElement = itemInCart.querySelector('[data-counter]');
+			counterElement.innerText = parseInt(counterElement.innerText) + parseInt(productInfo.counter);
+		} else {
+			// Если товара нет в корзине
+
+			// Собранные данные подставим в шаблон для товара в корзине
+			const cartItemHTML = `<div class="cart-item" data-id="${productInfo.id}">
+								<div class="cart-item__top">
+									<div class="cart-item__img">
+										<img src="${productInfo.imgSrc}" alt="${productInfo.title}">
+									</div>
+									<div class="cart-item__desc">
+										<div class="cart-item__title">${productInfo.title}</div>
+										
+
+										<!-- cart-item__details -->
+										<div class="cart-item__details">
+
+											<div class="items items--small counter-wrapper">
+												<div class="items__control" data-action="minus">-</div>
+												<div class="items__current" data-counter="">${productInfo.counter}</div>
+												<div class="items__control" data-action="plus">+</div>
+											</div>
+
+											<div class="price">
+												<div class="price__currency">${productInfo.price}</div>
+											</div>
+
+										</div>
+										<!-- // cart-item__details -->
+
+									</div>
+								</div>
+							</div>`;
+
+			// Отобразим товар в корзине
+			cartWrapper.insertAdjacentHTML('beforeend', cartItemHTML);
+		}
+
+		// Сбрасываем счетчик добавленного товара на "1"
+		card.querySelector('[data-counter]').innerText = '1';
+
+		// Отображение статуса корзины Пустая / Полная
+		toggleCartStatus();
+
+		// Пересчет общей стоимости товаров в корзине
+		calcCartPrice();
+    }
+});
